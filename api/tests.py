a705a49af90e2df84636b63bf2f167f47c75126f
Elicácio C. de Farias <elicaciocardoso@gmail.com>
2021-07-05 00:51:24 -0300

    test: adiciona testes para PackageUpdater

--- api/tests.py
+++ api/tests.py
@@ -1,54 +1,19 @@
 import pytest
 import json
 
-from api.services import PackageInfo, PackageRequest, PackageValidate
+from api.services import PackageInfo, PackageFinder, PackageValidator, PackageUpdater
 from rest_framework import serializers
 
 
 @pytest.fixture
-def package_request():
+def package_finder():
     package = {
         "name": "cronos",
         "packages": [
             {"name": "Django"},
         ],
     }
-    return PackageRequest(package, "error message")
-
-
-class TestPackageRequest:
-    def test__deve_retornar_uma_instancia_de_dicionario__quando_a_resposta_for_um_json_valido(
-        self,
-        mocker,
-        package_request,
-    ):
-        def get(*args):
-            return json.loads('{"key": "value"}')
-
-        mocker.patch.object(
-            package_request,
-            "get",
-            get,
-        )
-
-        result = package_request.get()
-        assert isinstance(result, dict)
-
-    def test__deve_lancar_excecao__quando_a_reposta_for_um_json_invalido(
-        self,
-        mocker,
-        package_request,
-    ):
-        def get(*args):
-            return json.loads('{"key", "value"}')
-
-        mocker.patch.object(
-            package_request,
-            "get",
-            get,
-        )
-        with pytest.raises(json.JSONDecodeError):
-            package_request.get()
+    return PackageFinder(package, "error message")
 
 
 @pytest.fixture
@@ -65,6 +30,55 @@ def response():
         },
     }
     return response
+
+
+@pytest.fixture
+def received_list():
+    return [
+        {
+            "name": "Django",
+            "version": "3.2.5",
+        },
+        {
+            "name": "psycopg2-binary",
+            "version": "2.9.1",
+        },
+    ]
+
+
+class TestPackageFinder:
+    def test__deve_retornar_uma_instancia_de_dicionario__quando_a_resposta_for_um_json_valido(
+        self,
+        mocker,
+        package_finder,
+    ):
+        def get(*args):
+            return json.loads('{"key": "value"}')
+
+        mocker.patch.object(
+            package_finder,
+            "get",
+            get,
+        )
+
+        result = package_finder.get()
+        assert isinstance(result, dict)
+
+    def test__deve_lancar_excecao__quando_a_reposta_for_um_json_invalido(
+        self,
+        mocker,
+        package_finder,
+    ):
+        def get(*args):
+            return json.loads('{"key", "value"}')
+
+        mocker.patch.object(
+            package_finder,
+            "get",
+            get,
+        )
+        with pytest.raises(json.JSONDecodeError):
+            package_finder.get()
 
 
 class TestPackageInfo:
@@ -106,10 +120,69 @@ class TestPackageInfo:
             package_info.get()
 
 
-class TestPackageValidate:
+class TestPackageValidator:
     def test__deve_retornar_uma_lista_vazia__quando_receber_uma_lista_de_pacotes_vazia(
         self,
     ):
-        package_validate = PackageValidate([], PackageRequest, PackageInfo, "error")
+        package_validate = PackageValidator([], PackageFinder, PackageInfo, "error")
         packages = package_validate.validate()
         assert len(packages) == 0
+
+
+class TestPackageUpdater:
+    def test__deve_retornar_a_lista_que_foi_recebida__quando_a_lista_atual_estiver_vazia(
+        self,
+        received_list,
+    ):
+
+        current_list = []
+        package_list = PackageUpdater(current_list, received_list)
+        result = package_list.update()
+        assert result == received_list
+
+    def test__deve_adicionar_o_pacote_na_lista_atual__quando_ele_não_existir(
+        self,
+        received_list,
+    ):
+        current_list = [
+            {
+                "name": "Django",
+                "version": "3.2.5",
+            },
+        ]
+        package_list = PackageUpdater(current_list, received_list)
+        result = package_list.update()
+        assert result == received_list
+
+    def test__deve_substituir_o_pacote_na_lista_atual__quando_a_versao_for_diferente_da_lista_recebida(
+        self,
+        received_list,
+    ):
+        current_list = [
+            {
+                "name": "Django",
+                "version": "3.2.2",
+            },
+            {
+                "name": "django-rest-swagger",
+                "version": "2.2.0",
+            },
+        ]
+
+        expected = [
+            {
+                "name": "Django",
+                "version": "3.2.5",
+            },
+            {
+                "name": "django-rest-swagger",
+                "version": "2.2.0",
+            },
+            {
+                "name": "psycopg2-binary",
+                "version": "2.9.1",
+            },
+        ]
+        package_updater = PackageUpdater(current_list, received_list)
+        result = package_updater.update()
+        assert result == expected
